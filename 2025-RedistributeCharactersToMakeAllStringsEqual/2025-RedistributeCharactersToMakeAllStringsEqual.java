27ecb40372347f3c34876f4db5879873c226cb48
SwathiN0614 <sn1947564@gmail.com>
2026-08-13 15:43:23 +0530

    Create 2025-RedistributeCharactersToMakeAllStringsEqual

---
+++ 2025-RedistributeCharactersToMakeAllStringsEqual/2025-RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,25 @@
+// Last updated: 8/13/2026, 3:43:17 PM
+class Solution {
+    public boolean makeEqual(String[] words) {
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        // Count the occurrences of each character
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        int n = words.length;
+
+        // Check if each character's count is a multiple of the number of strings
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            if (entry.getValue() % n != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
