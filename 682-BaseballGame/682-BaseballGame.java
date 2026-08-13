a2951aca4b6131cbe99e335da5449f4e289bd07c
SwathiN0614 <sn1947564@gmail.com>
2026-08-13 15:47:32 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.java
@@ -0,0 +1,24 @@
+// Last updated: 8/13/2026, 3:47:25 PM
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer>stack =new Stack<>();
+        for(String op:operations){
+            if(op.equals("+")){
+                int last=stack.pop();
+                int seclast=stack.peek();
+                stack.push(last);
+                stack.push(last+seclast);
+            }
+            else if(op.equals("D")){
+                stack.push(2*stack.peek());
+            }else if(op.equals("C")){
+                 stack.pop();
+            }else{
+                stack.push(Integer.parseInt(op));
+            }
+            }
+            int sum=0;
+            for(int score:stack)sum+=score;
+            return sum;
+        }
+    }
