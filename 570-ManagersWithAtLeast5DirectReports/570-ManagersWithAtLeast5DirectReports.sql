de8893595485e0dd750fb220f5211d186cdb45fc
SwathiN0614 <sn1947564@gmail.com>
2026-08-13 15:48:31 +0530

    Create 570-ManagersWithAtLeast5DirectReports

---
+++ 570-ManagersWithAtLeast5DirectReports/570-ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/13/2026, 3:48:24 PM
+# Write your MySQL query statement below
+
+Select m.name
+from employee as e
+inner join employee as m
+on e.managerId=m.id
+group by e.managerId 
+having count(e.id)>=5
