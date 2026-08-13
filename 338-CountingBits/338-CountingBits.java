75d7792f65eb7bfc5dd3dbdb84c28992147fcf0a
SwathiN0614 <sn1947564@gmail.com>
2026-08-13 15:49:58 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,19 @@
+// Last updated: 8/13/2026, 3:49:52 PM
+class Solution {
+    public int[] countBits(int n) {
+        //from 0 to n so n+1
+    int dp[]=new int[n+1];
+   
+    dp[0]=0;
+    for(int i=1;i<=n;i++){
+//check odd or even
+        if(i%2==0){
+//if even
+            dp[i]=dp[i/2];
+        }else
+//if odd
+        dp[i]=1+dp[i/2];
+    }
+    return dp;
+    }
+}
