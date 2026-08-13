fb190cb31b4befbb12ed35ece81b08c08f8002c4
SwathiN0614 <sn1947564@gmail.com>
2026-08-13 15:44:26 +0530

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.java
@@ -0,0 +1,22 @@
+// Last updated: 8/13/2026, 3:44:19 PM
+class Solution {
+    public int removePalindromeSub(String s) {
+        if(isPalindromic(s)) return 1;
+        return 2;
+    }
+
+    static boolean isPalindromic(String s){
+        int n = s.length();
+        int l = 0, r = n - 1;
+
+        while(l <= r){
+            if(s.charAt(l) != s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
