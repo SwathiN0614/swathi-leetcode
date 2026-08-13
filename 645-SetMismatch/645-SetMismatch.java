a52a93e9bc5937bfbc15aa4afd6a960ac6fa96ee
SwathiN0614 <sn1947564@gmail.com>
2026-08-13 15:47:44 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,15 @@
+// Last updated: 8/13/2026, 3:47:37 PM
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int N = nums.length, sum = N * (N + 1) / 2;
+        int[] ans = new int[2];
+        boolean[] seen = new boolean[N+1];
+        for (int num : nums) {
+            sum -= num;
+            if (seen[num]) ans[0] = num;
+            seen[num] = true;
+        }
+        ans[1] = sum + ans[0];
+        return ans;
+    }
+}
